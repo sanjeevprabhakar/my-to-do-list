b2f9c09bdd222789ffffcae9421244815ec5236b
sanjeevprabhakar <sanjeevprabhakar2021@gmail.com>
2023-09-10 08:35:46 -0600

    to do list work

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <p> MY TO DO LIST</p>
+    <input type = "text" />  <input type = "button" value ="Add"/> 
+    </>
+  );
+}
+
+export default App;
